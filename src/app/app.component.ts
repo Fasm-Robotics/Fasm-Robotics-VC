eecdb127df2a3ac7dfb866f69ff37fd933af7742
sabri <ouadahsabri2003@gmail.com>
2025-04-27 12:47:22 +0800

    Remove server-side configuration and routing setup.
    Deleted server-specific configurations and routes as they are no longer needed. Updated `app.component` to include `UrdfViewerComponent` and significantly simplified its HTML template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {UrdfViewerComponent} from './urdf-viewer/urdf-viewer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    UrdfViewerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
